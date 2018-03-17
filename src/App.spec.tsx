1e27e854bafb3f962f5a54b3e5687177c4c8e8b6
tsirlucas <tsirlucas@gmail.com>
2018-03-17 15:42:03 -0300

    chore(tests): fix tests setup

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
+jest.mock('react-native-google-signin', () => {});
+
 it('should render App without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
--- src/components/pages/Login/Login.container.spec.tsx
+++ src/components/pages/Login/Login.container.spec.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import {Login} from './Login.container';
 
+jest.mock('react-native-google-signin', () => {});
+
 it('should render Login page without crashing', () => {
   const rendered = renderer.create(<Login />).toJSON();
   expect(rendered).toBeTruthy();

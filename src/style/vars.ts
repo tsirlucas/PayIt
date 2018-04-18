30d564f450a19ca2c14dd9a74df75f36036cea99
tsirlucas <tsirlucas@gmail.com>
2018-04-18 01:06:28 -0300

    fix(colors): set unique primary color

--- src/style/vars.ts
+++ src/style/vars.ts
@@ -1,6 +1,4 @@
-import {Platform} from 'react-native';
-
-export const primaryColor = Platform.select({android: '#36AE81', ios: '#5FB97D'});
+export const primaryColor = '#36AE81';
 export const colors = {
   info: '#62B1F6',
   warning: '#f0ad4e',

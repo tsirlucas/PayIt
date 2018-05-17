4fb8863dbe3e67639e79cd8a64a37a4a912823ad
tsirlucas <tsirlucas@gmail.com>
2018-05-17 15:54:55 -0300

    chore(app): setup semantic release

--- ios/scripts/setVersion.sh
+++ ios/scripts/setVersion.sh
@@ -4,16 +4,11 @@ PROJECT_DIR="ios/PayIt"
 INFOPLIST_FILE="Info.plist"
 INFOPLIST_DIR="${PROJECT_DIR}/${INFOPLIST_FILE}"
 
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+PACKAGE_VERSION=$1
 
 BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_DIR}")
 BUILD_NUMBER=$(($BUILD_NUMBER + 1))
 
-
 # Update plist with new values
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${PACKAGE_VERSION#*v}" "${INFOPLIST_DIR}"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "${INFOPLIST_DIR}"
-
-git add "${INFOPLIST_DIR}"
-git commit -m "chore(ios): bump version [skip ci]"
-git push
